773134891e7c845b5191b18c2f78ccb393f0dbe0
SaadQasim19 <m.saadqasim7665@gmail.com>
2025-09-03 22:55:19 +0500

    Code : func to format time as hh:mm:ss

--- Code/main.ts
+++ Code/main.ts
@@ -6,3 +6,15 @@ const display = document.getElementById("display") as HTMLElement;
 const startBtn = document.getElementById("startBtn") as HTMLButtonElement;
 const stopBtn = document.getElementById("stopBtn") as HTMLButtonElement;
 const resetBtn = document.getElementById("resetBtn") as HTMLButtonElement;
+
+function timeToString(time: number): string {
+  const diffInHrs = Math.floor(time / 3600000);
+  const diffInMin = Math.floor((time % 3600000) / 60000);
+  const diffInSec = Math.floor((time % 60000) / 1000);
+
+  const formattedHrs = diffInHrs.toString().padStart(2, "0");
+  const formattedMin = diffInMin.toString().padStart(2, "0");
+  const formattedSec = diffInSec.toString().padStart(2, "0");
+
+  return `${formattedHrs}:${formattedMin}:${formattedSec}`;
+}
